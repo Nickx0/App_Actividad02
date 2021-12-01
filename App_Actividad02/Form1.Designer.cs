7a820fbe2321e949caf8d21a2ac5a7188a885763
NickX zXn <gamerplayernick714@hotmail.com>
2021-12-01 16:54:14 -0500

    Agregar archivos de proyecto.

---
+++ App_Actividad02/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿
+namespace App_Actividad02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMain = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnMain
+            // 
+            this.btnMain.Location = new System.Drawing.Point(39, 35);
+            this.btnMain.Name = "btnMain";
+            this.btnMain.Size = new System.Drawing.Size(169, 181);
+            this.btnMain.TabIndex = 0;
+            this.btnMain.Text = "Listar los numeros \r\ndel 1 al 50 \r\nmultiplos de 3";
+            this.btnMain.UseVisualStyleBackColor = true;
+            this.btnMain.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(57, 269);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(120, 40);
+            this.btnCerrar.TabIndex = 1;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 15;
+            this.listBox1.Location = new System.Drawing.Point(250, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(237, 274);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(542, 357);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnMain);
+            this.Name = "Form1";
+            this.Text = "App_Actividad02";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMain;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
